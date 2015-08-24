764c7bcc4e576c76b8d5e1a168a3b94d54cea064
Joshua Coats <joshu@fearchar.net>
2015-08-24 09:06:04 -0700

    password helper to select random words from /usr/share/dict/words

---
+++ PasswordHelper.hs
@@ -0,0 +1,61 @@
+import Control.Applicative ((<$>))
+import Control.Monad (foldM, liftM2)
+import Data.Maybe (fromMaybe)
+import System.Environment (getArgs)
+import System.IO (Handle, IOMode(ReadMode), hGetContents, openFile)
+import System.Random (getStdRandom, randomR)
+
+main :: IO ()
+main = do
+    (sampleSizeStr:_) <- getArgs
+    fh <- openFile "/usr/share/dict/words" ReadMode
+    theseWords <- readWords fh
+    chosenWords <- sample (read sampleSizeStr) theseWords
+    putStr (unlines chosenWords)
+  where
+    readWords :: Handle -> IO [String]
+    readWords = (fmap lines) . hGetContents
+
+sample :: Int                       -- the sample size to take
+       -> [a]                       -- the list of elements to sample from
+       -> IO [a]                    -- a random but non-repeating sample of elements from that list
+sample n xs =
+    fmap ((!!) xs) <$> indices
+  where
+    indices :: IO [Int]
+    indices = (fromMaybe []) <$> uniqueRandomRs n (0, ((length xs) - 1))
+
+uniqueRandomRs :: Int               -- the number of non-repeating random numbers to generate
+               -> (Int, Int)        -- the inclusive range of numbers to pass to randomR
+               -> IO (Maybe [Int])  -- a list of random numbers unless sample size exceeds range
+uniqueRandomRs sampleSize range
+    | sampleSize > rangeSize              = return Nothing
+    | otherwise                           = Just <$> foldM foldingFunction [] (take sampleSize [0..])
+  where
+    (rangeLow, rangeHigh) = range
+    rangeSize = rangeHigh - rangeLow + 1
+    foldingFunction :: [Int] -> Int -> IO [Int]
+    foldingFunction excluded i = let
+          decrRange = (flip (-)) i <$> range
+          accumWithExcluded = (:excluded)
+        in
+          accumWithExcluded <$> randomRNotInList decrRange excluded
+
+randomRNotInList :: (Int, Int)      -- the inclusive range in which to generate numbers with randomR
+                 -> [Int]           -- the list of numbers to be excluded
+                 -> IO Int          -- the new random number not in that list
+randomRNotInList range excluded
+    | rangeSize >= (length excluded)   = candidate
+    | otherwise                       = error("Excluded list is >= range of numbers ")
+  where
+    (rangeLow, rangeHigh) = range
+    rangeSize = rangeHigh - rangeLow + 1
+    candidate :: IO Int
+    candidate = liftM2 (+) offSet baseCandidate
+      where
+        baseCandidate :: IO Int
+        baseCandidate = getStdRandom (randomR range)
+        offSet :: IO Int
+        offSet = liftM2 count ((>=) <$> baseCandidate) (return excluded)
+        count :: (a -> Bool) -> [a] -> Int
+        count f = length . (filter f)
