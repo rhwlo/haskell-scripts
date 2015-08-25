a4776319166ab2135f668d20b72c62e745af08f0
Joshua Coats <joshu@fearchar.net>
2015-08-24 23:01:54 -0700

    Refactored count again

--- PasswordHelper.hs
+++ PasswordHelper.hs
@@ -61,4 +61,4 @@ randomRNotInList range excluded
 count :: (a -> Bool)                -- a predicate function to tell whether an element should count
       -> [a]                        -- a list of elements to potentially count
       -> Int                        -- the number of elements that matched the predicate function
-count p = foldl' (\accum x -> if (p x) then (accum + 1) else accum) 0
+count p = sum . (map (fromEnum . p))
